ba71752e725d016969d1407cee9eb0617d4d0ce3
wolfram <wolfram@wolframs-mbp-3.fritz.box>
2016-03-23 07:13:42 +0100

    new tests

--- src/test/java/de/illilli/opendata/service/gruenflaechen/koeln/ObjekttypFilterTest.java
+++ src/test/java/de/illilli/opendata/service/gruenflaechen/koeln/ObjekttypFilterTest.java
@@ -6,12 +6,17 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.filter.text.cql2.CQLException;
 import org.junit.Before;
 import org.junit.Test;
 import org.opengis.feature.simple.SimpleFeature;
+
+import com.vividsolutions.jts.geom.Geometry;
 
 public class ObjekttypFilterTest {
 
@@ -30,18 +35,43 @@ public class ObjekttypFilterTest {
 	}
 
 	@Test
-	public void test() throws IOException, CQLException {
-		ObjekttypFilter filter = new ObjekttypFilter(params, code);
-		SimpleFeatureCollection sfc = filter.getSimpleFeatureCollection();
+	public void testSpielplaetze() throws IOException, CQLException {
+		DataStore store = DataStoreFinder.getDataStore(params);
+		SimpleFeatureSource featureSource = store.getFeatureSource(store.getTypeNames()[0]);
+
+		ObjekttypFilter filter = new ObjekttypFilter(featureSource);
+		SimpleFeatureCollection sfc = filter.getSimpleFeatureCollection(FlaechentypEnum.SPIELPLATZ);
 		SimpleFeatureIterator iterator = sfc.features();
 
 		while (iterator.hasNext()) {
-
 			SimpleFeature feature = iterator.next();
+
 			System.out.println(feature.getAttribute(2) + ": " + feature.getAttribute(3) + ": " + feature.getAttribute(7)
 					+ " - " + feature.getAttribute(2));
 
 		}
+	}
+
+	@Test
+	public void testIntegrity() throws IOException, CQLException {
+		DataStore store = DataStoreFinder.getDataStore(params);
+		SimpleFeatureSource featureSource = store.getFeatureSource(store.getTypeNames()[0]);
+
+		SimpleFeatureCollection simpleFeatureCollection = featureSource.getFeatures();
+		SimpleFeatureIterator iterator = simpleFeatureCollection.features();
+
+		while (iterator.hasNext()) {
+			SimpleFeature feature = iterator.next();
+			Geometry geometry = (Geometry) feature.getDefaultGeometry();
+
+			if (geometry != null && !geometry.isValid()) {
+				System.out.println("Invalid Geoemtry: " + feature.getID() + "; GeometryType: "
+						+ geometry.getGeometryType() + "; SRID: " + geometry.getSRID() + "; getCoordinate :"
+						+ geometry.getCoordinate().toString());
+			}
+		}
+		featureSource.getDataStore().dispose();
+
 	}
 
 }
