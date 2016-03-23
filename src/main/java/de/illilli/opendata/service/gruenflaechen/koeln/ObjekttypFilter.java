04046170b148fc41c01a1a6d973cf466283d980b
wolfram <wolfram@wolframs-mbp-3.fritz.box>
2016-03-23 07:13:25 +0100

    remove deprecated methods

--- src/main/java/de/illilli/opendata/service/gruenflaechen/koeln/ObjekttypFilter.java
+++ src/main/java/de/illilli/opendata/service/gruenflaechen/koeln/ObjekttypFilter.java
@@ -1,10 +1,7 @@
 package de.illilli.opendata.service.gruenflaechen.koeln;
 
 import java.io.IOException;
-import java.util.Map;
 
-import org.geotools.data.DataStore;
-import org.geotools.data.DataStoreFinder;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.filter.text.cql2.CQL;
@@ -16,32 +13,8 @@ public class ObjekttypFilter {
 	private SimpleFeatureCollection features;
 	private SimpleFeatureSource featureSource;
 
-	/**
-	 * @deprecated
-	 * @return
-	 */
-	@Deprecated
-	public ObjekttypFilter(Map<String, Object> params, String code) throws IOException, CQLException {
-
-		DataStore store = DataStoreFinder.getDataStore(params);
-		SimpleFeatureSource featureSource = store.getFeatureSource(store.getTypeNames()[0]);
-
-		Filter filter = CQL.toFilter("Objekttyp > -1 ");
-		features = featureSource.getFeatures(filter);
-
-	}
-
 	public ObjekttypFilter(SimpleFeatureSource featureSource) {
 		this.featureSource = featureSource;
-	}
-
-	/**
-	 * @deprecated
-	 * @return
-	 */
-	@Deprecated
-	public SimpleFeatureCollection getSimpleFeatureCollection() {
-		return features;
 	}
 
 	public SimpleFeatureCollection getSimpleFeatureCollection(FlaechentypEnum flaechentyp)
